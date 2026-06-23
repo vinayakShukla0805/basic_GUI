2b129390a2c95dd04868895f0ba101f5f25fea42
vinayakShukla0805 <v87074@gmail.com>
2026-06-23 23:08:33 +0530

    register_gui_development

--- app.py
+++ app.py
@@ -21,4 +21,27 @@ class NLPApp:
 
         self.email_input = Entry(self.root,width=50)
         self.email_input.pack(pady=(5,10),ipady=4)
+
+        label2 = Label(self.root,text='Enter Password',width=40)
+        label2.pack(pady=(10,10))
+
+        self.password_input = Entry(self.root,width=50,show='*')
+        self.password_input.pack(pady=(5,10),ipady=4)
+
+        login_btn = Button(self.root,text='Login',width=30,height=2)
+        login_btn.pack(pady=(5,5))
+
+        label3 = Label(self.root,text='Not registered?',width=30)
+        label3.pack(pady=(10,5))
+
+        redirect_btn = Button(self.root,text='register',command=self.register_gui)
+        redirect_btn.pack()
+
+    def register_gui(self):
+        self.clear_gui()
+        print('Registration Open')
+
+    def clear_gui(self):
+        for i in self.root.pack_slaves():
+            i.destroy()
 nlp = NLPApp()
